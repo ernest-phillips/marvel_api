860c5d02bf514cbf73ac2e176cbda9c1b7da9e7e
ernest-phillips <ernestlphillips@gmail.com>
2018-09-09 07:51:40 -0700

    changes to js file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const URL = `https://gateway.marvel.com:443/v1/public/characters?orderBy=name&limit=12&apikey=dae95cc0861de65bc53fe1c135bc361b&offset=0
+const URL = `https://gateway.marvel.com:443/v1/public/characters?orderBy=name&limit=100&apikey=dae95cc0861de65bc53fe1c135bc361b&offset=0
 `
-const URL1 = `https://gateway.marvel.com:443/v1/public/characters?orderBy=name&limit=20&apikey=dae95cc0861de65bc53fe1c135bc361b&offset=13
+const URL1 = `https://gateway.marvel.com:443/v1/public/characters?orderBy=name&limit=12&apikey=dae95cc0861de65bc53fe1c135bc361b&offset=13
 `
 fetch(URL)
 .then(function(res)
@@ -10,28 +10,42 @@ fetch(URL)
     const query =  dataAsJson.data;
 
     readQuery(query);
+
   });
- 
+
 });
 
-function readQuery(query){
-    const list = query.results.map((item, index) => showHeroes(item));
+// function myFunction(){
+//   const query = {
+//     limt
+//     apikey
+//   }
+// $.getJSON(URL, query, callback)
+// }
+
+
+function readQuery(data){
+    //clear old data and then do this
+    $('.query-results').html()
+    const list = data.results.map((item, index) => showHeroes(item));
   }
 
 function showHeroes(item){
-    console.log(item.urls[0].url)
-     $('.query-results').append(`
+ let heroIMG = item.thumbnail.path
+
+  const heroData =   $('.query-results').append(`
      <div class="hero-viewed col-4">
   <img src="${item.thumbnail.path}/standard_fantastic.${item.thumbnail.extension}" alt="${item.name} portrait">
-  <a href="${item.urls[0].url}" ><h4 class="hero-name">${item.name}</h4></a>
+  <h4 class="hero-name">${item.name}</h4></a>
 </div>`
 
-)
-hideNotFound()
+
+) console.log(heroData.length())
+// hideNotFound(heroIMG)
 }
-  function hideNotFound(){
-    $("img:contains('image_not_available')").hide();
-  }
+  // function hideNotFound(heroIMG){
+  //   $(`${heroIMG}:contains("image_not_available")`).hide();
+  // }
 
 function nextPage(){
   $('#btn_next').on('click', function(){
